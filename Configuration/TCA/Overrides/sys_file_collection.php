17e2cf3eba5b013a23220d3d8e2392c2c338082a
Ephraim Härer <ephraim@ephespage.de>
2020-11-13 16:05:34 +0100

    added position of new field

--- Configuration/TCA/Overrides/sys_file_collection.php
+++ Configuration/TCA/Overrides/sys_file_collection.php
@@ -15,7 +15,6 @@
             'label' => 'LLL:EXT:reint_downloadmanager/Resources/Private/Language/locallang_db.xlf:sys_file_collection.description',
             'config' => [
                 'type' => 'text',
-                'enableRichtext' => true,
                 'cols' => 20,
                 'rows' => 5,
                 'eval' => 'null',
@@ -30,17 +29,21 @@
             'label' => 'LLL:EXT:reint_downloadmanager/Resources/Private/Language/locallang_db.xlf:sys_file_collection.description_frontend',
             'config' => [
                 'type' => 'text',
+                'enableRichtext' => true,
                 'cols' => 20,
                 'rows' => 5,
                 'eval' => 'null',
-                'placeholder' => '__row|uid_local|metadata|description',
-                'mode' => 'useOrOverridePlaceholder',
                 'default' => null,
             ]
         ],
     ];
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, $newSysCategoryColumns);
-
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+        'sys_file_collection',
+        'description_frontend',
+        '',
+        'after:description'
+    );
     /* order the file collections default by the sorting field */
     $GLOBALS['TCA'][$table]['ctrl']['default_sortby'] = 'ORDER BY sorting';
     $GLOBALS['TCA'][$table]['ctrl']['sortby'] = 'sorting';
