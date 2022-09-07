c2dbbff20c36c7577c1609c581c1713304143a3a
Ephraim Härer <ephraim.haerer@renolit.com>
2022-09-07 11:02:46 +0200

    added header field in content elements, updated output of content with Bootstrap 5

--- Configuration/TCA/Overrides/reintdownloadmanager_dmtopdownloads.php
+++ Configuration/TCA/Overrides/reintdownloadmanager_dmtopdownloads.php
@@ -15,8 +15,9 @@
         'types' => [
             $contentType => [
                 'showitem' => implode(',', [
-                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general',
-                    '--palette--;;general',
+                    '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general, '.
+                    '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general, '.
+                    '--palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.headers;headers',
                     'pi_flexform',
                     '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,--palette--;;frames,--palette--;;appearanceLinks,',
                     '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,--palette--;;language,',
