e67dd7edcdd09a5321c2bd1ae727cde3f69066cf
Ephraim Härer <ephraim.haerer@renolit.com>
2025-01-09 16:08:19 +0100

    basic changes for TYPO3 13.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,12 +10,12 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Download manager',
     'description' => 'A simple download manager with different views of file collections as downloadable lists.',
-    'version' => '4.0.1',
+    'version' => '5.0.0',
     'category' => 'plugin',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.15-12.4.99',
-            'php' => '8.1.0-8.2.99',
+            'typo3' => '13.4.0-13.4.99',
+            'php' => '8.2.0-8.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
