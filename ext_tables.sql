bfbebfe0ba17288c038dd455aae083b279aa93ca
Ephraim Härer <ephraim.haerer@renolit.com>
2025-01-16 21:41:01 +0100

    added ext_tables.sql to keep index of table in sql

---
+++ ext_tables.sql
@@ -0,0 +1,7 @@
+#
+# Add keys for table 'tx_reintdownloadmanager_domain_model_download'
+#
+CREATE TABLE tx_reintdownloadmanager_domain_model_download
+(
+    KEY `sys_file_uid` (`sys_file_uid`)
+);
